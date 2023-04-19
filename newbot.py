3854fab26f5da87344feb7e3ca8fb7716c580c0e
Kirill <kir.tearo@yandex.ru>
2023-04-19 17:49:47 +0300

    Add global dictionary for keeping all information about each user

--- newbot.py
+++ newbot.py
@@ -5,9 +5,10 @@ import telebot
 from telebot import types
 
 from file_parser import parce_file
+from user_status import UserStatus
 
 
-def file_saver(user_answers, true_answers, name, res):
+def file_saver(id_):
     """
     Save the user's results locally in a file.
     
@@ -18,20 +19,20 @@ def file_saver(user_answers, true_answers, name, res):
     res -- user's score
     """
     global quests_all
-    with open(f"answers_{name}.txt", "w", encoding="utf-8") as f:
-        print(f'{name}\n{time.ctime()}\nОбщий результат: {res} {round(res*100 / len(quests_all),2)}%\n', file=f)
+    global status
+    with open(f"answers_{status[id_].name}.txt", "w", encoding="utf-8") as f:
+        print(f'{status[id_].name}\n{time.ctime()}\nОбщий результат: {status[id_].res} {round(status[id_].res*100 / len(quests_all),2)}%\n', file=f)
         for num, quest in enumerate(quests_all):
             print(f"Вопрос {num+1}", file=f)
-            print(f"Ответ пользователя: {user_answers[quest.title]}", file=f)
-            print(f"Правильный ответ: {true_answers[quest.title]}", file=f)
+            print(f"Ответ пользователя: {status[id_].user_ans[quest.title]}", file=f)
+            print(f"Правильный ответ: {status[id_].true_ans[quest.title]}", file=f)
 
 
 def ask_question_sm(message, qst):
     """Send a question to the user, indicating the buttons for the answer under the message"""
-    global last_quest
-    global last_bot_message
-    global last_user_message
-    global last_markup
+    global status
+    id_ = message.from_user.id
+    
     if qst.type_of_q=='S':
         msg = qst.title + "\nНажмите верный ответ"
     else:
@@ -46,17 +47,19 @@ def ask_question_sm(message, qst):
         mark.append(types.InlineKeyboardButton('Закончить выбор',callback_data = '@'))
     markup.add(*mark)
     
-    last_markup = markup
-    last_quest = qst
-    last_bot_message = bot.send_message(message.from_user.id, msg,reply_markup=markup)
-    last_user_message = message
+    status[id_].last_markup = markup
+    status[id_].last_quest = qst
+    status[id_].last_bot_message = bot.send_message(message.from_user.id, msg,reply_markup=markup)
+    status[id_].last_user_message = message
     
 def ask_question_o(message, qst):
     """Send a question to the user that requires sending a message from the user"""
-    global last_quest
-    global last_user_message
-    last_quest = qst
-    last_user_message = message
+    global status
+    id_ = message.from_user.id
+    
+    status[id_].last_quest = qst
+    status[id_].last_user_message = message
+    
     msg = qst.title + "\nНапечатайте верный ответ"
     bot.send_message(message.from_user.id, msg)
     bot.register_next_step_handler(message, handle_o)
@@ -64,13 +67,14 @@ def ask_question_o(message, qst):
 
 def send_quest(message):
     """Select the next question to send to the user and calling the sending functions"""
-    global quests
+    global status
+    id_ = message.from_user.id
 
-    if not(len(quests)):
+    if not(len(status[id_].quests)):
         get_result(message)
         return
-    quest = quests[0]
-    del quests[0]
+    quest = status[id_].quests[0]
+    del status[id_].quests[0]
     if quest.type_of_q in ('S','M'):
         return ask_question_sm(message, quest)
     if quest.type_of_q=='O':
@@ -79,27 +83,25 @@ def send_quest(message):
 
 print('Hello! Welcome to tester as teacher. Be ready with Telegram bot API, questions-file')
 
-API_KEY = input("Enter Bot API key, given by Telegram Bot Father    ").strip()
-#API_KEY = '6033741551:AAFW7B3eVpEe95aBg81S0cH8t6Cy4XtjocA'
-filedir = input("Enter name of quiz-file (it should be in programm path!)    ")
-#filedir = 'MYQUEST.txt'
+#API_KEY = input("Enter Bot API key, given by Telegram Bot Father    ").strip()
+API_KEY = '6033741551:AAFW7B3eVpEe95aBg81S0cH8t6Cy4XtjocA'
+#filedir = input("Enter name of quiz-file (it should be in programm path!)    ")
+filedir = 'MYQUEST.txt'
 while 1:
     try:
         filedir = open(filedir, 'r')
         break
     except FileNotFoundError:
         filedir = input("Wrong directory. Try again.    ")
-new_entering_msg = input("Enter custom hello-message, if it's needed.    ").strip()
-#new_entering_msg=None
+#new_entering_msg = input("Enter custom hello-message, if it's needed.    ").strip()
+new_entering_msg=None
 
 quests_all = parce_file(filedir)
 quests = quests_all.copy()
 rd.shuffle(quests)
 
-user_answers = dict()
-true_answers = dict()
-usr_res = 0
-user_name = ''
+status = dict()
+
 
 print('>>BOT STARTED TO WORK<<')
 bot = telebot.TeleBot(API_KEY)
@@ -117,7 +119,7 @@ def check_answer(quest, user_answer):
         return set(user_answer) == quest.answer
     return set([user_answer.strip().lower()])==quest.answer
 
-def handle_answer(quest, user_answer):
+def handle_answer(id_, quest, user_answer):
     """
     Perform verification, save the user response. Start the next question function
     
@@ -125,24 +127,23 @@ def handle_answer(quest, user_answer):
     quest -- question
     user_answer -- question's answer via user
     """
-    global usr_res
-    global last_user_message
-    global user_answers
-    global true_answers
+    global status 
     #print(check_answer(quest,user_answer))
-    usr_res += check_answer(quest,user_answer)
-    if last_quest.type_of_q in {'S','M'}:
-        user_answers[quest.title] = set(user_answer)
+    status[id_].res += check_answer(quest,user_answer)
+    
+    if status[id_].last_quest.type_of_q in {'S','M'}:
+        status[id_].user_ans[quest.title] = set(user_answer)
     else:
-        user_answers[quest.title] = set([user_answer.strip().lower()])
-    true_answers[quest.title] = last_quest.answer
+        status[id_].user_ans[quest.title] = set([user_answer.strip().lower()])
+    status[id_].true_ans[quest.title] = status[id_].last_quest.answer
     
-    send_quest(last_user_message)
+    send_quest(status[id_].last_user_message)
     
-def callback_handle_s(user_ans):
+def callback_handle_s(id_, user_ans):
     """TODO"""
-    global last_bot_message
-    global last_quest
+    global status 
+    last_bot_message = status[id_].last_bot_message
+    last_quest = status[id_].last_quest
 
     bot.edit_message_reply_markup(chat_id=last_bot_message.chat.id, 
                                       message_id=last_bot_message.id, 
@@ -151,14 +152,15 @@ def callback_handle_s(user_ans):
     bot.edit_message_text(chat_id=last_bot_message.chat.id, 
                               message_id=last_bot_message.id,
                               text=last_bot_message.text + f'\n\nВаш ответ: {user_ans}')
-    handle_answer(last_quest, [user_ans])                          
+    handle_answer(id_, last_quest, [user_ans])                          
     
     
-def callback_handle_m(user_ans):
+def callback_handle_m(id_, user_ans):
     """TODO"""
-    global last_bot_message
-    global last_markup
-    global last_quest
+    global status 
+    last_bot_message = status[id_].last_bot_message
+    last_markup = status[id_].last_markup
+    last_quest = status[id_].last_quest
     
     row = last_bot_message.text.split('\n')[-1]
     if row.startswith('Вы выбрали:'):
@@ -173,7 +175,7 @@ def callback_handle_m(user_ans):
         bot.edit_message_reply_markup(chat_id=last_bot_message.chat.id, 
                                       message_id=last_bot_message.id, 
                                       reply_markup='')
-        handle_answer(last_quest,ans) 
+        handle_answer(id_, last_quest, ans) 
         return
         
     new_text = last_bot_message.text
@@ -192,36 +194,45 @@ def callback_handle_m(user_ans):
         for i in ans[1:]:
             new_text+=' | '+i
         
-    message = bot.edit_message_text(chat_id=last_bot_message.chat.id, 
+    status[id_].last_bot_message = bot.edit_message_text(chat_id=last_bot_message.chat.id, 
                               message_id=last_bot_message.id,
                               text=new_text,
                               reply_markup=last_markup)
-    last_bot_message = message
+  
     
 def handle_o(message):
     """TODO"""
-    global last_quest 
-    handle_answer(last_quest, message.text)
+    global status 
+    id_ = message.from_user.id
+    handle_answer(id_, status[id_].last_quest, message.text)
  
     
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call): 
     """TODO"""
     if call.data:
-        global last_quest
+        global status
+        last_quest = status[call.from_user.id].last_quest
+        id_ = call.from_user.id
         if last_quest.type_of_q == 'S':
-            callback_handle_s(call.data)
+            callback_handle_s(id_, call.data)
         elif last_quest.type_of_q == 'M':
-            callback_handle_m(call.data)
-        elif last_quest.type_of_q == 'O':
-            callback_handle_o(call.data)
+            callback_handle_m(id_, call.data)
+        #elif last_quest.type_of_q == 'O':
+        #    callback_handle_o(id_, call.data)
                 
 
 @bot.message_handler(content_types=['text'])
 def start(message):
     """Start to host bot, request user's name."""
-    #send_quest(message)
-    #return
+    global status
+    print(message.from_user.id)
+    quests = quests_all.copy()
+    rd.shuffle(quests)
+    status[message.from_user.id] = UserStatus(name='', res=0, user_ans=dict(), true_ans=dict(), last_bot_message=None, 
+                                              last_user_message=None, last_markup=None, quests=quests, last_quest=None)
+    send_quest(message)
+    return
     
     if message.text == '/start':
         if new_entering_msg:
@@ -238,8 +249,8 @@ def start(message):
 
 def get_name(message):
     """TODO"""
-    global user_name
-    user_name = message.text
+    global status
+    status[message.from_user.id].name = message.text
     MSG = f"Всего в тесте будет {len(quests)} вопросов трёх типов: \n\
             \tс множественным выбором, \n\
             \tс определенным ответом, \n\
@@ -250,14 +261,9 @@ def get_name(message):
     
 def get_result(message):
     """TODO"""
-    global user_name
-    global user_res
-    global user_answers
-    global true_answers
+    global status
     
-    bot.send_message(message.from_user.id, f'Ваш результат: {usr_res}')
-    file_saver(user_answers, true_answers, user_name, usr_res)
-    tmp_questions = quests_all.copy()
-    rd.shuffle(tmp_questions)
+    bot.send_message(message.from_user.id, f'Ваш результат: {status[message.from_user.id].res}')
+    file_saver(message.from_user.id)
 
 bot.polling(none_stop=True, interval=0)
