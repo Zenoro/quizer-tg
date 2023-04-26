a13a908feac392c07bc6934a7acc04068f6eb8e2
Kirill <kir.tearo@yandex.ru>
2023-04-26 20:25:04 +0300

    The code is fixed according to pep8

--- file_parser.py
+++ file_parser.py
@@ -2,17 +2,14 @@ import re
 from dataclasses import dataclass
 
 
-
-
 @dataclass
 class Question:
-	type_of_q: str
-	title: str
-	var: list | None
-	answer: set
-	
-	
-	
+    type_of_q: str
+    title: str
+    var: list | None
+    answer: set
+
+
 def handle_s_question(text):
     """Reading a question with one answer"""
     title = text[0][3:]
@@ -26,7 +23,8 @@ def handle_s_question(text):
     for i in ans:
         answer.append(temp[i])
     return Question('S', title, var, set(answer))
-    
+
+
 def handle_m_question(text):
     """Reading a question with multiple answers"""
     title = text[0][3:]
@@ -40,14 +38,15 @@ def handle_m_question(text):
     for i in ans:
         answer.append(temp[i])
     return Question('M', title, var, set(answer))
-    
+
+
 def handle_o_question(text):
     """Reading a question with an answer for input"""
     title = text[0][3:]
     answer = text[1][3:].strip().lower()
     return Question('O', title, None, set([answer]))
-	
-	
+
+
 def parce_file(fd):
     """
     The function of reading questions and answers from a file
@@ -57,30 +56,23 @@ def parce_file(fd):
     """
     text = ''
     for line in fd:
-    	text+=line
-    text = text.replace('\n','@')
+        text += line
+    text = text.replace('\n', '@')
     questions = []
     while len(text):
-        match = re.search('/[SMO].*?/Q.*?@',text)
+        match = re.search('/[SMO].*?/Q.*?@', text)
         if not match:
             break
         questions.append(match[0])
         end = match.end()
-        text=text[end:]
+        text = text[end:]
     quenstions = [q.split('@')[0:-1] for q in questions]
     res = []
     for quest in quenstions:
         if quest[0].startswith('/S'):
-            #continue
             res.append(handle_s_question(quest))
         if quest[0].startswith('/M'):
-            #continue
             res.append(handle_m_question(quest))
         if quest[0].startswith('/O'):
             res.append(handle_o_question(quest))
-    #for i in res:
-    
-    #    print(i)
     return res
-
-
--- newbot.py
+++ newbot.py
@@ -18,7 +18,9 @@ def file_saver(id_):
     global quests_all
     global status
     with open(f"answers_{status[id_].name}.txt", "w", encoding="utf-8") as f:
-        print(f'{status[id_].name}\n{time.ctime()}\nОбщий результат: {status[id_].res} {round(status[id_].res*100 / len(quests_all),2)}%\n', file=f)
+        res = status[id_].res
+        print(f'{status[id_].name}\n{time.ctime()}\nОбщий результат: {res} {round(res*100 / len(quests_all),2)}%\n',
+              file=f)
         for num, quest in enumerate(quests_all):
             print(f"Вопрос {num+1}", file=f)
             print(f"Ответ пользователя: {status[id_].user_ans[quest.title]}", file=f)
@@ -29,34 +31,35 @@ def ask_question_sm(message, qst):
     """Send a question to the user, indicating the buttons for the answer under the message"""
     global status
     id_ = message.from_user.id
-    
-    if qst.type_of_q=='S':
+
+    if qst.type_of_q == 'S':
         msg = qst.title + "\nНажмите верный ответ"
     else:
         msg = qst.title + "\nВыберите верные ответы"
     markup = types.InlineKeyboardMarkup()
     mark = []
-    
+
     for i in qst.var:
-        button = types.InlineKeyboardButton(i,callback_data = i)
+        button = types.InlineKeyboardButton(i, callback_data=i)
         mark.append(button)
-    if qst.type_of_q=='M':
-        mark.append(types.InlineKeyboardButton('Закончить выбор',callback_data = '@'))
+    if qst.type_of_q == 'M':
+        mark.append(types.InlineKeyboardButton('Закончить выбор', callback_data='@'))
     markup.add(*mark)
-    
+
     status[id_].last_markup = markup
     status[id_].last_quest = qst
-    status[id_].last_bot_message = bot.send_message(message.from_user.id, msg,reply_markup=markup)
+    status[id_].last_bot_message = bot.send_message(message.from_user.id, msg, reply_markup=markup)
     status[id_].last_user_message = message
-    
+
+
 def ask_question_o(message, qst):
     """Send a question to the user that requires sending a message from the user"""
     global status
     id_ = message.from_user.id
-    
+
     status[id_].last_quest = qst
     status[id_].last_user_message = message
-    
+
     msg = qst.title + "\nНапечатайте верный ответ"
     bot.send_message(message.from_user.id, msg)
     bot.register_next_step_handler(message, handle_o)
@@ -72,9 +75,9 @@ def send_quest(message):
         return
     quest = status[id_].quests[0]
     del status[id_].quests[0]
-    if quest.type_of_q in ('S','M'):
+    if quest.type_of_q in ('S', 'M'):
         return ask_question_sm(message, quest)
-    if quest.type_of_q=='O':
+    if quest.type_of_q == 'O':
         return ask_question_o(message, quest)
 
 
@@ -100,6 +103,7 @@ status = dict()
 print('>>BOT STARTED TO WORK<<')
 bot = telebot.TeleBot(API_KEY)
 
+
 def check_answer(quest, user_answer):
     """
     Check the correctness of the answer (depending on the question).
@@ -108,10 +112,11 @@ def check_answer(quest, user_answer):
     quest -- question
     user_answer -- question's answer via user
     """
-    if quest.type_of_q in {'S','M'}:
-        #print(set(user_answer), quest.answer)
+    if quest.type_of_q in {'S', 'M'}:
+        # print(set(user_answer), quest.answer)
         return set(user_answer) == quest.answer
-    return set([user_answer.strip().lower()])==quest.answer
+    return set([user_answer.strip().lower()]) == quest.answer
+
 
 def handle_answer(id_, quest, user_answer):
     """
@@ -122,18 +127,19 @@ def handle_answer(id_, quest, user_answer):
     quest -- question
     user_answer -- question's answer via user
     """
-    global status 
-    #print(check_answer(quest,user_answer))
-    status[id_].res += check_answer(quest,user_answer)
-    
-    if status[id_].last_quest.type_of_q in {'S','M'}:
+    global status
+    # print(check_answer(quest,user_answer))
+    status[id_].res += check_answer(quest, user_answer)
+
+    if status[id_].last_quest.type_of_q in {'S', 'M'}:
         status[id_].user_ans[quest.title] = set(user_answer)
     else:
         status[id_].user_ans[quest.title] = set([user_answer.strip().lower()])
     status[id_].true_ans[quest.title] = status[id_].last_quest.answer
-    
+
     send_quest(status[id_].last_user_message)
-    
+
+
 def callback_handle_s(id_, user_ans):
     """
     Process the callback of a button with the user's answer to a question with one answer
@@ -142,20 +148,20 @@ def callback_handle_s(id_, user_ans):
     id_ -- the user's key to the dictionary status
     user_ans -- users's answer on the last question
     """
-    global status 
+    global status
     last_bot_message = status[id_].last_bot_message
     last_quest = status[id_].last_quest
 
-    bot.edit_message_reply_markup(chat_id=last_bot_message.chat.id, 
-                                      message_id=last_bot_message.id, 
-                                      reply_markup='')
-                                      
-    bot.edit_message_text(chat_id=last_bot_message.chat.id, 
-                              message_id=last_bot_message.id,
-                              text=last_bot_message.text + f'\n\nВаш ответ: {user_ans}')
-    handle_answer(id_, last_quest, [user_ans])                          
-    
-    
+    bot.edit_message_reply_markup(chat_id=last_bot_message.chat.id,
+                                  message_id=last_bot_message.id,
+                                  reply_markup='')
+
+    bot.edit_message_text(chat_id=last_bot_message.chat.id,
+                          essage_id=last_bot_message.id,
+                          text=last_bot_message.text + f'\n\nВаш ответ: {user_ans}')
+    handle_answer(id_, last_quest, [user_ans])
+
+
 def callback_handle_m(id_, user_ans):
     """
     Process the callback of a button with the user's answer to a question with several answers
@@ -164,58 +170,57 @@ def callback_handle_m(id_, user_ans):
     id_ -- the user's key to the dictionary status
     user_ans -- users's answer on the last question
     """
-    global status 
+    global status
     last_bot_message = status[id_].last_bot_message
     last_markup = status[id_].last_markup
     last_quest = status[id_].last_quest
-    
+
     row = last_bot_message.text.split('\n')[-1]
     if row.startswith('Вы выбрали:'):
         ans = row.split(' | ')
         ans[0] = ans[0][ans[0].index(':')+2:]
-        if ans[0]=='':
-            ans=[]
+        if ans[0] == '':
+            ans = []
     else:
-        ans=[]
-        
+        ans = []
+
     if user_ans == "@":
-        bot.edit_message_reply_markup(chat_id=last_bot_message.chat.id, 
-                                      message_id=last_bot_message.id, 
+        bot.edit_message_reply_markup(chat_id=last_bot_message.chat.id,
+                                      message_id=last_bot_message.id,
                                       reply_markup='')
-        handle_answer(id_, last_quest, ans) 
+        handle_answer(id_, last_quest, ans)
         return
-        
+
     new_text = last_bot_message.text
     rows = new_text.split('\n')
-    last_row = rows[-1]
     new_text = ''
     for i in rows[0:-1]:
-        new_text+=i
-    new_text+='\n\nВы выбрали: '
+        new_text += i
+    new_text += '\n\nВы выбрали: '
     if user_ans in ans:
         ans.remove(user_ans)
     else:
         ans.append(user_ans)
     if len(ans):
-        new_text+=ans[0]
+        new_text += ans[0]
         for i in ans[1:]:
-            new_text+=' | '+i
-        
-    status[id_].last_bot_message = bot.edit_message_text(chat_id=last_bot_message.chat.id, 
-                              message_id=last_bot_message.id,
-                              text=new_text,
-                              reply_markup=last_markup)
-  
-    
+            new_text += ' | '+i
+
+    status[id_].last_bot_message = bot.edit_message_text(chat_id=last_bot_message.chat.id,
+                                                         message_id=last_bot_message.id,
+                                                         text=new_text,
+                                                         reply_markup=last_markup)
+
+
 def handle_o(message):
     """Сheck the user's message for the correctness of the answer to the question"""
-    global status 
+    global status
     id_ = message.from_user.id
     handle_answer(id_, status[id_].last_quest, message.text)
- 
-    
+
+
 @bot.callback_query_handler(func=lambda call: True)
-def callback_inline(call): 
+def callback_inline(call):
     """The callback function for the user to acquire buttons under the questions"""
     if call.data:
         global status
@@ -225,9 +230,7 @@ def callback_inline(call):
             callback_handle_s(id_, call.data)
         elif last_quest.type_of_q == 'M':
             callback_handle_m(id_, call.data)
-        #elif last_quest.type_of_q == 'O':
-        #    callback_handle_o(id_, call.data)
-                
+
 
 @bot.message_handler(content_types=['text'])
 def start(message):
@@ -236,11 +239,9 @@ def start(message):
     print(message.from_user.id)
     quests = quests_all.copy()
     rd.shuffle(quests)
-    status[message.from_user.id] = UserStatus(name='', res=0, user_ans=dict(), true_ans=dict(), last_bot_message=None, 
+    status[message.from_user.id] = UserStatus(name='', res=0, user_ans=dict(), true_ans=dict(), last_bot_message=None,
                                               last_user_message=None, last_markup=None, quests=quests, last_quest=None)
-    send_quest(message)
-    return
-    
+
     if message.text == '/start':
         # print(message.from_user.id)
         if new_entering_msg:
@@ -266,12 +267,14 @@ def get_name(message):
             Напишите ответ для начала теста."
     bot.send_message(message.from_user.id, MSG)
     bot.register_next_step_handler(message, send_quest)
-    
+
+
 def get_result(message):
     """Completing the test, saving the results"""
     global status
-    
+
     bot.send_message(message.from_user.id, f'Ваш результат: {status[message.from_user.id].res}')
     file_saver(message.from_user.id)
+
 
 bot.polling(none_stop=True, interval=0)
