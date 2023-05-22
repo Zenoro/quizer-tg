89e00f30ba68d3c1027b6d3d089dea14ce484dea
Kirill <kir.tearo@yandex.ru>
2023-05-22 21:34:11 +0300

    Add tool for building wheel

---
+++ QuizerBot/__main__.py
@@ -0,0 +1,4 @@
+from . import newbot
+
+if __name__ == '__main__':
+    newbot.main()
---
+++ QuizerBot/user_status.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from telebot.types import Message
+from .file_parser import Question
+
+
+@dataclass
+class UserStatus:
+    name: str
+    res: int
+
+    user_ans: dict
+    true_ans: dict
+
+    last_bot_message: Message
+    last_user_message: Message
+    last_markup: list
+
+    quests: list
+    last_quest: Question
